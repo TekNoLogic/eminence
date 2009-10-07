24aa4a6fc438aec517fcdf9691e00288565f00a6
tekkub <tekkub@gmail.com>
2009-10-07 02:24:38 -0600

    Possibly fix up bossblock (I've not seen any spam)

--- BossBlock.lua
+++ BossBlock.lua
@@ -1,29 +1,21 @@
 
-local function IsSpam(text)
-	if type(text) ~= "string" then return end
-	if string.find(text, "%*%*%*") then return true end
-end
-
-
-local function filter(self, event, msg)
-	if select(4, GetBuildInfo()) < 30100 then msg = self end
-	if IsSpam(msg) then return true end
-end
-
+local function IsSpam(text) if type(text) == "string" and string.find(text, "%*%*%*") then return true end end
+local function filter(self, event, msg) if IsSpam(msg) then return true end end
 for _,chan in pairs{"CHAT_MSG_WHISPER", "CHAT_MSG_RAID", "CHAT_MSG_RAID_WARNING", "CHAT_MSG_RAID_LEADER"} do ChatFrame_AddMessageEventFilter(chan, filter) end
 
 
-local o = RaidNotice_AddMessage
-local RaidWarningFrame = RaidWarningFrame
-function RaidNotice_AddMessage(self, message, ...)
-	if self == RaidWarningFrame and IsSpam(message) then return end
-	return o(self, message, ...)
-end
+-- Removing this for the moment, as the only places it is called are
+-- RaidWarningFrame_OnEvent and RaidBossEmoteFrame_OnEvent.
+-- We block RaidWarningFrame_OnEvent below, and we want to allow RaidBossEmoteFrame_OnEvent
+--~ local o = RaidNotice_AddMessage
+--~ local RaidWarningFrame = RaidWarningFrame
+--~ function RaidNotice_AddMessage(self, message, ...)
+--~ 	if self == RaidWarningFrame and IsSpam(message) then return end
+--~ 	return o(self, message, ...)
+--~ end
 
 
 -- Gotta hook this bitch or we'll get the noise but no message
+-- Also have to directly override the OnEvent of the frame because of the new crummy XML handler thingy
 local o2 = RaidWarningFrame_OnEvent
-function RaidWarningFrame_OnEvent(self, event, message, ...)
-	if IsSpam(message) then return end
-	return o2(self, event, message, ...)
-end
+RaidWarningFrame:SetScript("OnEvent", function (self, event, message, ...) if IsSpam(message) then return else	return o2(self, event, message, ...) end end)
